391caf2a4dce59fec4eeaf5c32c8c3953dabd25d
boricle <boricles@gmail.com>
2011-10-08 21:20:03 +0200

    index file included

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var router = require("./lib/router");
 var requestHandlers = require("./lib/requestHandlers");
 
 var handle = {}
+handle["/"] = requestHandlers.start;
 handle["/explore"] = requestHandlers.explore;
 
 init.start(router.route, handle);
