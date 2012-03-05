b0304abd58a28c23ae5ee880ac3f34229988bbb1
boricle <boricles@gmail.com>
2012-03-05 13:22:07 +0100

    align to nodester code

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 vocabexpress is a simple tool for extracting the vocabulary of a given dataset.
 Current url: http://vocab-express.nodester.com/
-
-TODO:
-- Finish defining the input/output
-- Improve the layout of initial result page   
+  
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var requestHandlers = require("./lib/requestHandlers");
 var handle = {}
 handle["/"] = requestHandlers.start;
 handle["/explore"] = requestHandlers.explore;
+handle["/exploreTerm"] = requestHandlers.exploreTerm;
 
 init.start(router.route, handle);
 
